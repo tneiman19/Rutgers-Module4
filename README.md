d1228cd2163194e6d615fc8638cf57199800fa07
Tzvi Neiman <tneiman19@gmail.com>
2023-01-19 01:36:13 -0500

    Added the five questions, moved question object into an array so it will be easier to work with the objects, small CSS changes.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Rutgers-Module4
 JavaScript Quiz
+
+Making note to give credit to [InterviewBit](https://www.interviewbit.com/javascript-mcq/) for the Javascript questions and answers used in this project. 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,85 @@
-var answerArea = document.getElementById('answerArea')
+var questionArea = document.getElementById("questionArea");
+var answerArea = document.getElementById("answerArea");
 
-var questionNumber = 1;
-var currentQuestion = 'question' + questionNumber
+var index = 0;
 
-console.log(currentQuestion)
-
-var question1 = {
-  question: "What color is the Sky?",
-  answers: {
-    Green: false,
-    Blue: true,
-    Purple: false,
-    "None of the above": false,
+var questionArray = [
+  {
+    question:
+      "Which of the following keywords is used to define a variable in Javascript?",
+    answers: {
+      var: false,
+      let: false,
+      "var and let": true,
+      "None of the above": false,
+    },
   },
+  {
+    question: "Javascript is an _______ language?",
+    answers: {
+      "Object-Oriented": true,
+      "Object-Based": false,
+      Procedural: false,
+      "None of the above": false,
+    },
+  },
+  {
+    question:
+      "When an operator’s value is NULL, the typeof returned by the unary operator is:",
+    answers: {
+      Boolean: false,
+      undefined: false,
+      Object: true,
+      Integer: false,
+    },
+  },
+  {
+    question: "How can a datatype be declared to be a constant type?",
+    answers: {
+      const: true,
+      var: false,
+      let: false,
+      constant: false,
+    },
+  },
+  {
+    question:
+      "What keyword is used to check whether a given property is valid or not?",
+    answers: {
+      lies: false,
+      "is in": false,
+      exists: false,
+      in: true,
+    },
+  },
+];
+
+// This function will take an array name and index and display the question and answers for that index on the DOM
+var renderQuestion = function (array, index) {
+  var questionH1 = document.createElement("h1");
+  questionH1.textContent = questionArray[index].question;
+  questionArea.appendChild(questionH1);
+
+  for (var key in array[index].answers) {
+    //console.log(key);
+    var newButton = document.createElement("button");
+    newButton.textContent = key;
+    newButton.setAttribute("class", "answerButton");
+    newButton.setAttribute("data-correct", array[index].answers[key]);
+    answerArea.appendChild(newButton);
+    //console.log(array[index].answers[key])
+  }
 };
 
+renderQuestion(questionArray, index);
 
 
+//We may use this in a later function to clear out the current children and replace with the new questions children
+// function removeAllChildNodes(parent) {
+//     while (parent.firstChild) {
+//         parent.removeChild(parent.firstChild);
+//     }
+// }
 
-function answerObject(obj){
-    for (const key in obj.answers) {
-        console.log(key, obj.answers[key]);
-        var newParagraph = document.createElement("button");
-        newParagraph.innerHTML = key;
-        newParagraph.setAttribute("class", "answerButton");
-        newParagraph.setAttribute("data-correct", obj.answers[key]);
-        answerArea.appendChild(newParagraph);
-      }
-}
-
-answerObject(question1)
+// removeAllChildNodes(questionArea)
+// removeAllChildNodes(answerArea)
