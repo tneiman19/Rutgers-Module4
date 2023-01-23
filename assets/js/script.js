bed927b5aa7089cb52071be05a0a46b1d0d30b19
Tzvi Neiman <tneiman19@gmail.com>
2023-01-22 20:58:35 -0500

    Added: Functions to show or gide elements during different parts of the game, a function that is called when time is up or all questions are answered, a function to check the users answers and update the score accordingly,a reset function that will reset the variables and text content if the user starts a new game.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,14 @@
+var gameArea = document.getElementById("gameArea");
 var questionArea = document.getElementById("questionArea");
 var answerArea = document.getElementById("answerArea");
-
+var correctSpan = document.getElementById("correctSpan");
+var incorrectSpan = document.getElementById("incorrectSpan");
+var timerSpan = document.getElementById("timerSpan");
+var startButton = document.getElementById("startButton");
+var interval = 0;
+var wins = 0;
+var loss = 0;
+var timer = 0;
 var index = 0;
 
 var questionArray = [
@@ -54,6 +62,26 @@ var questionArray = [
   },
 ];
 
+//This function is the function that starts the game.  When the user clicks the start button we we start runing this code/functions
+startButton.addEventListener("click", timerFunction);
+function timerFunction() {
+  reset();
+  renderQuestion(questionArray, index);
+  changeSectionVisibility(gameArea, "visible");
+  changeSectionVisibility(startButton, "hidden");
+  timer = 60;
+  interval = setInterval(function startTimer() {
+    if (timer > 0) {
+      timer--;
+      timerSpan.textContent = timer;
+    }
+    if (timer == 0) {
+      clearInterval(interval);
+      gameOver();
+    }
+  }, 1000);
+}
+
 // This function will take an array name and index and display the question and answers for that index on the DOM
 var renderQuestion = function (array, index) {
   var questionH1 = document.createElement("h1");
@@ -71,15 +99,72 @@ var renderQuestion = function (array, index) {
   }
 };
 
-renderQuestion(questionArray, index);
+//This function is used each time an answer is clicked on to check if the answer is correct.
+answerArea.addEventListener("click", function (event) {
+  var validClick = event.target.getAttribute("class");
+  var answerCorrect = event.target.getAttribute("data-correct");
+  console.log(
+    "The target class attribute is:",
+    validClick,
+    "and the answer is:",
+    answerCorrect
+  );
+  if (validClick !== "answerButton") {
+    return;
+  } else {
+    updateScore(answerCorrect);
+    removeAllChildNodes(questionArea);
+    removeAllChildNodes(answerArea);
+    index++;
+    if (index < questionArray.length) {
+      renderQuestion(questionArray, index);
+    } else {
+      gameOver();
+    }
+  }
+});
 
+//This function is used to update the score variables
+function updateScore(text) {
+  if (text === "true") {
+    wins++;
+    correctSpan.textContent = wins;
+    // alert("Correct!");
+  } else {
+    loss++;
+    incorrectSpan.textContent = loss;
+    // alert("Incorrect");
+  }
+}
 
-//We may use this in a later function to clear out the current children and replace with the new questions children
-// function removeAllChildNodes(parent) {
-//     while (parent.firstChild) {
-//         parent.removeChild(parent.firstChild);
-//     }
-// }
+//Use this function to clear out the current question and answer, so we can replace the area with the new question and answers
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
 
-// removeAllChildNodes(questionArea)
-// removeAllChildNodes(answerArea)
+//This function is used to show/hide elements
+function changeSectionVisibility(section, visibility) {
+  section.style.visibility = visibility;
+}
+
+//Use this function when the users runs out of time or questions
+function gameOver() {
+  removeAllChildNodes(questionArea);
+  removeAllChildNodes(answerArea);
+  clearInterval(interval);
+  changeSectionVisibility(gameArea, "hidden");
+  changeSectionVisibility(startButton, "visible");
+}
+
+//This function is used to reset the variable values when the user tries to start a new game.
+function reset() {
+  interval = 0;
+  wins = 0;
+  loss = 0;
+  timer = 0;
+  index = 0;
+  correctSpan.textContent = wins;
+  incorrectSpan.textContent = loss;
+}
